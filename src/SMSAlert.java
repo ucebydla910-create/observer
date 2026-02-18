ed14a6b2c78b2b64dda5ebc85087b30335f1c747
майор <ucebydla910@gmail.com>
2026-02-18 10:21:35 +0500

    Update SMSAlert.java

--- src/SMSAlert.java
+++ src/SMSAlert.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+package observers;
 
-public class SMSAlert {
+import observer.Observer;
+import observer.ServerMonitor;
+
+/**
+ * Оповещение по SMS.
+ * Срабатывает при статусе OFFLINE.
+ */
+public class SMSAlert implements Observer {
+    private final String phone = "+7-999-123-45-67";
+
+    @Override
+    public void update(ServerMonitor server) {
+        if ("OFFLINE".equals(server.getStatus())) {
+            System.out.printf("📱 SMS to %s: Server %s is DOWN!\n",
+                    phone, server.getName());
+        }
+    }
 }
