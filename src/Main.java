d54978ff13f222497b271dd603470b33ddc12c84
майор <ucebydla910@gmail.com>
2026-02-18 10:19:53 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+import observer.ServerMonitor;
+import observers.*;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("🚀 Запуск системы мониторинга...\n");
+
+        ServerMonitor server = new ServerMonitor("web-server-01");
+
+        EmailAlert emailAlert = new EmailAlert();
+        SMSAlert smsAlert = new SMSAlert();
+        DashboardLogger dashboardLogger = new DashboardLogger();
+        PerformanceRecorder performanceRecorder = new PerformanceRecorder(); // дополнительный
+
+        server.attach(emailAlert);
+        server.attach(smsAlert);
+        server.attach(dashboardLogger);
+        server.attach(performanceRecorder);
+        System.out.println();
+
+        server.setCpuLoad(92.0);
+        server.setMemoryUsage(45.0);
+        server.setStatus("OFFLINE");
+        System.out.println();
+
+        server.detach(emailAlert);
+        System.out.println("(EmailAlert больше не получает уведомления)\n");
+
+        server.setCpuLoad(95.0);
+        server.setStatus("ONLINE");
+        System.out.println();
+
+        System.out.println("✅ Работа завершена. История CPU сохранена в metrics.csv");
+    }
 }
